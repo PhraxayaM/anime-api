89bbf14cfcfe7cd1c57a0e8bf97cd02fd9f7b424
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-02-28 00:20:19 -0800

    added request

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app =  express()
 const request = require("request")
-const port = 8080
+const port = 8080 
+const url = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json"
 
-app.get("/", (req, res) => {
-    res.json("Welcome to the car api")
+app.get("/", (req, res) => { 
+    // res.json("Welcome to the car api")
+    request(url, function (error, response, body) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode); 
+      console.log('body:', body); 
+    });
 })
 
 app.listen(port, () => {
